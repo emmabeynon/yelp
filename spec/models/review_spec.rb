a7f138e0ad7ddc2b039bf240630afcc6954e30b2
emmabeynon <emma.beynon@gmail.com>
2016-01-18 21:13:00 +0000

    Added validation to the review model to ensure that only ratings between 1 and 5 can be submitted.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,9 @@ describe Review do
   it { should belong_to(:restaurant) }
 
   it { should belong_to(:restaurant).dependent(true) }
+
+  it 'is invalid if the rating is more than 5' do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
 end
