ef492752b111cd2bb7d662a84dd888156581541e
emmabeynon <emma.beynon@gmail.com>
2016-01-19 13:47:14 +0000

    Added Facebook login option using OmniAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :restaurants do
     resources :reviews
+  end
+
+  devise_scope :user do
+    delete 'sign out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path
   end
 
   root to: "restaurants#index"
