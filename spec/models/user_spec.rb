91ddb9d1faae961a7e73a36e424f6b4305ce3927
emmabeynon <emma.beynon@gmail.com>
2016-01-20 14:26:30 +0000

    Added reviewed_restaurants as an association to User.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe User, type: :model do
   it { should have_many(:restaurants) }
   it { should have_many(:reviews) }
+  it { is_expected.to have_many :reviewed_restaurants }
 end
